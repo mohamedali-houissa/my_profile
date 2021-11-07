15c0ca15042f8dc2db42406465059ba0fdfb9524
mohamedali-houissa <92219834+mohamedali-houissa@users.noreply.github.com>
2021-11-07 21:22:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function on() {
+    console.log("coucou")
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    console.log("bye")
+    document.getElementById("overlay").style.display = "none";
+  }
+
